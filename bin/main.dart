871645231958a5d0841b509fc5dd6826cf2f2c91
pradeep8891 <pradeep.kumar5@rsystems.com>
2024-01-18 13:00:37 +0530

    add calculator

---
+++ bin/main.dart
@@ -0,0 +1,6 @@
+import 'package:dart_learn/calculator/calculator_repo.dart';
+// import 'package:dart_learn/dart_learn.dart' as dart_learn;
+
+void main(List<String> arguments) {
+  calc();
+}
---
+++ lib/calculator/calculator_repo.dart
@@ -0,0 +1,84 @@
+import 'dart:io';
+
+abstract class ICalculator {
+  void sum();
+  void sub();
+  void mul();
+  void div();
+  void sqrt();
+  void power();
+}
+
+class Calculator extends ICalculator {
+  final int x;
+  final int y;
+  Calculator({required this.x, required this.y});
+  @override
+  void div() {
+    print('result -> ${x / y}');
+  }
+
+  @override
+  void mul() {
+    print('result -> ${x * y}');
+  }
+
+  @override
+  void power() {
+    print('result -> ${x + y}');
+  }
+
+  @override
+  void sqrt() {
+    print('result -> ${x + y}');
+  }
+
+  @override
+  void sub() {
+    print('result -> ${x - y}');
+  }
+
+  @override
+  void sum() {
+    print('result -> ${x + y}');
+  }
+
+  @override
+  String toString() {
+    return '{x:$x,y:$y}';
+  }
+}
+
+void calc() {
+  print("Enter first number");
+  int x = int.parse(stdin.readLineSync()!);
+  print("Enter first number");
+  int y = int.parse(stdin.readLineSync()!);
+  final calculator = Calculator(x: x, y: y);
+  print("enter operator (+,-,*,/)");
+  String op = stdin.readLineSync()!;
+  switch (op) {
+    case '+':
+      calculator.sum();
+      more();
+    case '-':
+      calculator.sub();
+      more();
+    case '*':
+      calculator.mul();
+      more();
+    case '/':
+      calculator.div();
+      more();
+    default:
+      exit(0);
+  }
+}
+
+void more() {
+  print("continue... press y/yes, for stop press any key");
+  String value = stdin.readLineSync()!;
+  if (value == "y") {
+    return calc();
+  }
+}
